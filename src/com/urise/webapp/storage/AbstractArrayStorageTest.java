f408c69f92ac3c889f720f6c878374c7c4d5646e
Herman <gpahomov@inbox.ru>
2020-04-10 14:30:33 +0500

    HW4 AbstractArrayStorageTest added.

---
+++ src/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,57 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AbstractArrayStorageTest {
+    private Storage storage = new ArrayStorage();
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    @BeforeEach
+    void setUp() {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void size() {
+        Assertions.assertEquals(3, storage.size());
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    public void getNotExist() {
+        Assertions.assertThrows(NotExistStorageException.class, () -> storage.get("dummy"));
+    }
+
+    @Test
+    void getAll() {
+
+    }
+}
