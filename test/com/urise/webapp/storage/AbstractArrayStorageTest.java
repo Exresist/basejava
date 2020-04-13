0a9d520c31b6f16712c4fe5fec3cc81061abfae6
Herman <gpahomov@inbox.ru>
2020-04-14 00:01:40 +0500

    HW4 AbstractArrayStorageTest, ArrayStorageTest, SortedArrayStorageTest, MainReflection.

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -11,12 +11,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public abstract class AbstractArrayStorageTest {
-    protected Storage storage;
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
     }
 
+    protected Storage storage;
 
     private static final String UUID_1 = "uuid1";
     private static final Resume RESUME_1 = new Resume(UUID_1);
@@ -33,10 +33,11 @@ public abstract class AbstractArrayStorageTest {
         storage.save(RESUME_3);
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() throws Exception {
-        storage.delete("uuid2");
+        storage.delete(UUID_2);
         assertEquals(2, storage.size());
+        storage.get(UUID_2);
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -46,8 +47,15 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void save() throws Exception {
-        storage.save(new Resume());
+        String UUID_4 = "uuid4";
+        Resume RESUME_4 = new Resume(UUID_4);
+        storage.save(RESUME_4);
         assertEquals(4, storage.size());
+        try {
+            storage.get(UUID_4);
+        } catch (Exception e) {
+            fail();
+        }
     }
 
     @Test(expected = StorageException.class)
